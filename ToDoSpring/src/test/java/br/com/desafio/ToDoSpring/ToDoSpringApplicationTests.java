133a352519deeab5c52e3a122050816b49e1a7c0
Lucas Paiva <lucasgmpaiva1@gmail.com>
2020-10-24 10:09:31 -0300

    Criando projeto Spring

---
+++ ToDoSpring/src/test/java/br/com/desafio/ToDoSpring/ToDoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.ToDoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
