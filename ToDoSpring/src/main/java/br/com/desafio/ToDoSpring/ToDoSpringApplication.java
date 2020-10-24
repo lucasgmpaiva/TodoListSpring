b8af1a25f0a8a3ac51d72245d972455a45aaf1fb
Lucas Paiva <lucasgmpaiva1@gmail.com>
2020-10-24 10:31:28 -0300

    Mapeando classes MVC na aplicação

--- ToDoSpring/src/main/java/br/com/desafio/ToDoSpring/ToDoSpringApplication.java
+++ ToDoSpring/src/main/java/br/com/desafio/ToDoSpring/ToDoSpringApplication.java
@@ -2,8 +2,14 @@ package br.com.desafio.ToDoSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"br.com.desafio.ToDoSpring.model"})
+@EnableJpaRepositories(basePackages = {"br.com.desafio.ToDoSpring.repository"})
+@ComponentScan(basePackages = {"br.com.desafio.ToDoSpring.control"})
 public class ToDoSpringApplication {
 
 	public static void main(String[] args) {
