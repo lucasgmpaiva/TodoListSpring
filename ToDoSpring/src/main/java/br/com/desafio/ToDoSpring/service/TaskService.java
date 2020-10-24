25a114d2345050b49e4ca6ff7f48d68d8e1e1899
Lucas Paiva <lucasgmpaiva1@gmail.com>
2020-10-24 14:40:48 -0300

    Adicionando anotação service

--- ToDoSpring/src/main/java/br/com/desafio/ToDoSpring/service/TaskService.java
+++ ToDoSpring/src/main/java/br/com/desafio/ToDoSpring/service/TaskService.java
@@ -1,8 +1,11 @@
 package br.com.desafio.ToDoSpring.service;
 
+import org.springframework.stereotype.Service;
+
 import br.com.desafio.ToDoSpring.model.Task;
 import br.com.desafio.ToDoSpring.repository.TaskRepository;
 
+@Service
 public class TaskService {
 	
 	private TaskRepository taskRepository;
