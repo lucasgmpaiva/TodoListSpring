31ea985eb0324788335d9242c6a1b5de315a99b8
Lucas Paiva <lucasgmpaiva1@gmail.com>
2020-10-24 10:17:30 -0300

    Criando classe model da Task

---
+++ ToDoSpring/src/main/java/br/com/desafio/ToDoSpring/model/Task.java
@@ -0,0 +1,48 @@
+package br.com.desafio.ToDoSpring.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name="name", nullable = false)
+	private String name;
+	
+	@Column(name="description")
+	private String description;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	
+	
+}
